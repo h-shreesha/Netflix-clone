61deb6654add2bbf1ed91da03ebe78786de8cb47
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-30 22:28:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1 className='text-3xl font-bold bg-red-200'>hello</h1>
     </div>
   );
 }

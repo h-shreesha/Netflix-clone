6eb03edf660ff5205094567343d3bce5cde7fc87
Shreesha H R <shreeshahr9303@gmail.com>
2023-04-04 09:41:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl font-bold bg-red-200'>hello</h1>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import requests from '../Requests';
+
+const Main = () => {
+    const [movies, setMovies] = useState([]);
+    const movie = movies[Math.floor(Math.random() * movies.length)];
+
+    useEffect(() => {
+        axios.get(requests.requestPopular).then((response) => {
+            setMovies(response.data.results);
+        });
+    }, []);
+    console.log(movies);
+    return (
+        <div className="w-full h-[550px] text-white">
+            <div className="w-full h-full">
+                <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+                <img
+                    className="w-full h-full object-cover"
+                    src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+                    alt={movies?.title}
+                />
+                <div className="absolute w-full top-[20%] p-4 md:p-8">
+                    <h1>{movie?.title}</h1>
+                    <div>
+                        <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+                            Play
+                        </button>
+                        <button className="border text-white  border-gray-300 py-2 px-5  ml-4">
+                            Watch Later
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;

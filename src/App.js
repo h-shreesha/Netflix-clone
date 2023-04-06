918a9453368ec3e8c940da8fae7ef05bf33f079c
Shreesha H R <shreeshahr9303@gmail.com>
2023-04-06 21:33:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-
 import './App.css';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+        children: [
+            {
+                path: '/',
+                element: <Main />,
+            },
+        ],
+    },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Main/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
+

584709d02bd73cdaa9c5e0f76fc40482c2880b7c
Shreesha H R <shreeshahr9303@gmail.com>
2023-04-07 22:39:20 +0530

    authentication

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,13 @@ import Main from '../components/Main'
 import Row from '../components/Row'
 import requests from '../Requests'
 import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      {/* <Outlet /> */}
         <Main/>
         <Row rowId = '1' title='Upcoming' fetchURL = {requests.requestUpcoming} />
         <Row rowId = '2' title='Popular' fetchURL = {requests.requestPopular} />
